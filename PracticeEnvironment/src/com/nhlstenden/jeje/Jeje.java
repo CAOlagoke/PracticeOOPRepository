de36d0c437f8dc2131e8d2f5d4552c212a33c28a
Chris <ayomide.olagoke@student.nhlstenden.com>
2023-11-06 09:02:08 +0100

    Practice

---
+++ PracticeEnvironment/src/com/nhlstenden/jeje/Jeje.java
@@ -0,0 +1,5 @@
+package com.nhlstenden.jeje;
+
+public class Jeje {
+
+}
---
+++ PracticeEnvironment/test/com/nhlstenden/jeje/JejeTest.java
@@ -0,0 +1,16 @@
+package com.nhlstenden.jeje;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JejeTest {
+
+    @BeforeEach
+    void setup(){
+
+        assertThrows(IndexOutOfBoundsException.class, () -> );
+    }
+
+}
